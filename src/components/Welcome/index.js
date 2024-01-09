b12d11993bd79045b31f5a57340e9634e50e2a41
shaikparvin <shaikparvin807@gmail.com>
2024-01-09 07:19:54 +0000

    first commit

---
+++ src/components/Welcome/index.js
@@ -0,0 +1,29 @@
+import {Component} from 'react'
+import './index.css'
+
+class Welcome extends Component {
+  state = {isSubscribed: false}
+
+  buttonText = () => {
+    this.setState(prevState => ({isSubscribed: !prevState.isSubscribed}))
+  }
+  getButtonText = () => {
+    const {isSubscribed} = this.state
+    return isSubscribed ? 'Subscribed' : 'Subscribe'
+  }
+
+  render() {
+    const buttonText = this.getButtonText()
+    return (
+      <div className="bg-container">
+        <h1 className="heading">Welcome</h1>
+        <p className="paragraph">Thank you! Happy Learning</p>
+        <button type="button" className="button" onClick={this.buttonText}>
+          {buttonText}
+        </button>
+      </div>
+    )
+  }
+}
+
+export default Welcome
